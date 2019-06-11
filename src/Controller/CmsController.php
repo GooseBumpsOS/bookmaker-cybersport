ae908c9d0336d78bf3a7adfcc1815737b931afd2
Georgy <goosebumpsmail2@gmail.com>
2019-06-12 00:32:52 +0300

    Добавлена возможность добавлять посты через CMS

--- src/Controller/CmsController.php
+++ src/Controller/CmsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 define('password', '$2y$10$2QlKFibkLUq5N71PyAhhrug6JyycE/0XpEx4pGw0DfEjybEe7.sWO');
 
+use App\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -24,15 +25,12 @@ class CmsController extends AbstractController
             return $this->redirectToRoute('cms');
 
         if ($request->request->count() != 0)
-            if ($request->request->get('login') == 'toor' && password_verify($request->request->get('password'), password)){
+            if ($request->request->get('login') == 'toor' && password_verify($request->request->get('password'), password)) {
                 $session->start();
                 $session->set('password', $request->request->get('password'));
+                return $this->redirectToRoute('cms');
 
             } else $wrongPassOrLogin = true;
-
-
-
-
 
         return $this->render('cms/login.html.twig', [
             'wrongPassOrLogin' => $wrongPassOrLogin,
@@ -42,15 +40,57 @@ class CmsController extends AbstractController
     /**
      * @Route("/cms", name="cms")
      */
-    public function cms()
+    public function cms(Request $request)
     {
         $session = new Session();
 
         if (!password_verify($session->get('password'), password))
-           return $this->redirectToRoute('cms-login');
+            return $this->redirectToRoute('cms-login');
 
-        return $this->render('dump.html.twig', [
-            'var' => 'OK',
+        if ($request->request->count() != 0)
+        {
+            $db = new News();
+
+            $db->setDate(new \DateTime(date("d-M-Y")));
+            $db->setImg($request->request->get('img'));
+            $db->setText($request->request->get('text'));
+            $db->setTitle($request->request->get('title'));
+            $db->setSlug($this->trasnlate($request->request->get('title')));
+
+            $this->getDoctrine()->getManager()->persist($db);
+            $this->getDoctrine()->getManager()->flush();
+
+
+        }
+
+        $em = $this->getDoctrine()->getManager()->getRepository(News::class);
+
+        return $this->render('cms/cms.html.twig', [
+            'rows' => $em->getByPage(1),
         ]);
+    }
+
+    private function trasnlate($str){
+
+        $tr = array(
+            "А"=>"A","Б"=>"B","В"=>"V","Г"=>"G",
+            "Д"=>"D","Е"=>"E","Ж"=>"J","З"=>"Z","И"=>"I",
+            "Й"=>"Y","К"=>"K","Л"=>"L","М"=>"M","Н"=>"N",
+            "О"=>"O","П"=>"P","Р"=>"R","С"=>"S","Т"=>"T",
+            "У"=>"U","Ф"=>"F","Х"=>"H","Ц"=>"TS","Ч"=>"CH",
+            "Ш"=>"SH","Щ"=>"SCH","Ъ"=>"","Ы"=>"YI","Ь"=>"",
+            "Э"=>"E","Ю"=>"YU","Я"=>"YA","а"=>"a","б"=>"b",
+            "в"=>"v","г"=>"g","д"=>"d","е"=>"e","ж"=>"j",
+            "з"=>"z","и"=>"i","й"=>"y","к"=>"k","л"=>"l",
+            "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
+            "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h",
+            "ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"y",
+            "ы"=>"yi","ь"=>"'","э"=>"e","ю"=>"yu","я"=>"ya",
+            "."=>"_"," "=>"_","?"=>"_","/"=>"_","\\"=>"_",
+            "*"=>"_",":"=>"_","\*"=>"_","\""=>"_","<"=>"_",
+            ">"=>"_","|"=>"_"
+        );
+        return strtr($str,$tr);
+
     }
 }
