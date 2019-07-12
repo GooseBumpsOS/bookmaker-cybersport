e93ae239e173109c6d6db1894b87917a0accee21
Georgy <goosebumpsmail2@gmail.com>
2019-07-13 00:15:34 +0300

    Сделан раздел с бонусами

--- src/Controller/BonusController.php
+++ src/Controller/BonusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Bonus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,16 @@ class BonusController extends AbstractController
      */
     public function index()
     {
+
+        $bg_img_array = ['https://cdn.pixabay.com/photo/2017/05/08/02/22/game-2294201_960_720.jpg', 'https://cdn.pixabay.com/photo/2018/04/11/17/11/background-3311042_960_720.jpg', 'https://cdn.pixabay.com/photo/2018/02/28/21/44/technology-3189176_960_720.jpg', 'https://cdn.pixabay.com/photo/2019/07/06/20/31/joystick-4321216_960_720.jpg', 'https://cdn.pixabay.com/photo/2019/04/15/11/42/fortnite-4129124_960_720.jpg', 'https://cdn.pixabay.com/photo/2017/06/29/10/28/games-2453777_960_720.jpg'];
+
+        $em = $this->getDoctrine()->getManager()->getRepository(Bonus::class);
+
+        $data = $em->findAll();
+
         return $this->render('bonus/index.html.twig', [
-            'controller_name' => 'BonusController',
+            'dbData' => $data,
+            'bgImg' => $bg_img_array,
         ]);
     }
 }
---
+++ src/Entity/Bonus.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BonusRepository")
+ */
+class Bonus
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     */
+    private $logo;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $preText;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $link;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPreText(): ?string
+    {
+        return $this->preText;
+    }
+
+    public function setPreText(string $preText): self
+    {
+        $this->preText = $preText;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+}
